21cf72e521267b2b97b6284def1d63d69b4472f3
adityapd1228 <ar.aditya@gmail.com>
2025-07-30 21:43:58 -0400

    Delete submittal_analyzer.py

--- submittal_analyzer.py
+++
@@ -1,5 +0,0 @@
-import streamlit as st
-
-st.set_page_config(page_title="📄 Submittal Analyzer", layout="wide")
-st.title("📄 Submittal Analyzer")
-st.markdown("This module is under development. Stay tuned!")
