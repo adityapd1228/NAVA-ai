742e18fa83c3fbda644c1c43521e9d6ff24e4554
adityapd1228 <ar.aditya@gmail.com>
2025-07-30 21:43:34 -0400

    Delete xer_parser.py

--- xer_parser.py
+++
@@ -1,5 +0,0 @@
-import streamlit as st
-
-st.set_page_config(page_title="🛠️ XER File Parser", layout="wide")
-st.title("🛠️ XER File Parser")
-st.markdown("This module is under development. Stay tuned!")
